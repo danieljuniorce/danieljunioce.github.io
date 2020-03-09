fab8257d905655c982d63bd6730359b4c0890037
danieljuniorce <danieljuniorce@hotmail.com>
2020-03-08 22:47:07 -0300

    Updates

---
+++ precache-manifest.c7a7119bfd2cc68bec6ab76e1e7b6501.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e174fac88d57789f9b862ac8438da0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dbc6aacc5dadd74ffd0c",
+    "url": "/portfolio/static/js/2.22684d6c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfolio/static/js/2.22684d6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23b33617d8daebb832dc",
+    "url": "/portfolio/static/js/main.11f56d5c.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
